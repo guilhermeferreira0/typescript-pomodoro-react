dbf767341c0420d0387c06350f96910463ffe37c
Guilherme Ferreira <guilherme.fsilva23@gmail.com>
2023-12-29 10:03:28 -0300

    Config iniciais

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+
+  root: true,
+};
